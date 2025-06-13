b49c8543d3c4268ae1d07d2a83c1bf06100c9a08
Mark Smith <judy@judy.co.uk>
2025-06-13 12:28:07 +0100

    Trivial reformat.

--- template/{{project_slug}}/src/{{package_name}}/_version.py
+++ template/{{project_slug}}/src/{{package_name}}/_version.py
@@ -24,9 +24,7 @@ def _get_hatch_version():
 
     pyproject_toml = locate_file(__file__, "pyproject.toml")
     if pyproject_toml is None:
-        raise RuntimeError(
-            "pyproject.toml not found although hatchling is installed"
-        )
+        raise RuntimeError("pyproject.toml not found although hatchling is installed")
     root = os.path.dirname(pyproject_toml)
     metadata = ProjectMetadata(root=root, plugin_manager=PluginManager())
     # Version can be either statically set in pyproject.toml or computed dynamically:
